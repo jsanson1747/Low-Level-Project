bd50c953c1322dacf53977650a36a047adfbce45
Jacob Sanson <jsanson1747@gmail.com>
2023-01-31 22:34:41 -0500

    Added Color

--- webgl-demo.js
+++ webgl-demo.js
@@ -18,17 +18,25 @@ function main() {
     //Vertex shader program
     const vsSource = `
         attribute vec4 aVertexPosition;
+        attribute vec4 aVertexColor;
+
         uniform mat4 uModelViewMatrix;
         uniform mat4 uProjectionMatrix;
-        void main(){
+
+        varying lowp vec4 vColor;
+
+        void main(void){
             gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+            vColor = aVertexColor;
         }
     `;
 
     //Fragment shader program
     const fsSource = `
-        void main(){
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        varying lowp vec4 vColor;
+
+        void main(void){
+            gl_FragColor = vColor;
         }
     `;
 
@@ -43,6 +51,7 @@ function main() {
         program: shaderProgram,
         attribLocations: {
             vertexPositions: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
         },
         uniformLocations: {
             projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
